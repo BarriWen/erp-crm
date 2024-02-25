165adf86d86c2e25d1a289e4a8771f60773dd8b6
BarriWen <wenzelongkyrie@outlook.com>
2024-02-25 01:49:05 -0500

    User Rsa public key api

--- zl-bilibili-api/src/main/java/com/zelong/bilibili/api/UserApi.java
+++ zl-bilibili-api/src/main/java/com/zelong/bilibili/api/UserApi.java
@@ -1,7 +1,10 @@
 package com.zelong.bilibili.api;
 
+import com.zelong.bilibili.domain.JsonResponse;
 import com.zelong.bilibili.service.UserService;
+import com.zelong.bilibili.service.utils.RSAUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +12,10 @@ public class UserApi {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/rsa-pks")
+    public JsonResponse<String> getRsaPublicKey() {
+        String pk = RSAUtil.getPublicKeyStr();
+        return new JsonResponse<>(pk);
+    }
 }
